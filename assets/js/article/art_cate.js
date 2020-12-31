8c6b0f2d50e44e2aee4b498e52a965de515a1e4f
周周 <353034337@qq.com>
2020-12-31 16:51:58 +0800

    文章编辑和发布

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,104 @@
+$(function () {
+  var layer = layui.layer;
+  initArtCateList();
+
+  // 为添加类别按钮绑定点击事件
+  var indexAdd = null;
+  $("#btnAddCate").on("click", function () {
+    indexAdd = layer.open({
+      type: 1,
+      area: ["500px", "250px"],
+      title: "添加文章分类",
+      content: $("#dialog-add").html(),
+    });
+  });
+
+  // 通过代理的形式，为 form-add 表单绑定 submit 事件
+  $("body").on("submit", "#form-add", function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/my/article/addcates",
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("新增分类失败！");
+        }
+        initArtCateList();
+        layer.msg("新增分类成功！");
+        // 根据索引，关闭对应的弹出层
+        layer.close(indexAdd);
+      },
+    });
+  });
+  //  修改类别
+  var indexEdit = null;
+  var form = layui.form;
+  $("tbody").on("click", ".btn-edit", function () {
+    // 弹出一个修改文章分类信息的层
+    indexEdit = layer.open({
+      type: 1,
+      area: ["500px", "250px"],
+      title: "修改文章分类",
+      content: $("#dialog-edit").html(),
+    });
+
+    var id = $(this).attr("data-id");
+    // 发起请求获取对应分类的数据
+    $.ajax({
+      method: "GET",
+      url: "/my/article/cates/" + id,
+      success: function (res) {
+        form.val("form-edit", res.data);
+      },
+    });
+  });
+
+  $("body").on("submit", "#form-edit", function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/my/article/updatecate",
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("更新分类数据失败！");
+        }
+        layer.msg("更新分类数据成功！");
+        layer.close(indexEdit);
+        initArtCateList();
+      },
+    });
+  });
+
+  // 删除 事件
+  $("tbody").on("click", ".btn-delete", function () {
+    var id = $(this).attr("data-id");
+    // 提示用户是否要删除
+    layer.confirm("确认删除?", { icon: 3, title: "提示" }, function (index) {
+      $.ajax({
+        method: "GET",
+        url: "/my/article/deletecate/" + id,
+        success: function (res) {
+          if (res.status !== 0) {
+            return layer.msg("删除分类失败！");
+          }
+          layer.msg("删除分类成功！");
+          layer.close(index);
+          initArtCateList();
+        },
+      });
+    });
+  });
+
+  function initArtCateList() {
+    $.ajax({
+      method: "GET",
+      url: "/my/article/cates",
+      success: function (res) {
+        var htmlStr = template("tpl-table", res);
+        $("tbody").html(htmlStr);
+      },
+    });
+  }
+});
